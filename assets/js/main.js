457d160a31aafa203cbd3c4e1aef19d874915522
Christopher Amanor <kriskit7@gmail.com>
2020-11-20 22:55:49 +0000

    Fix issues with responsiveness of pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,26 @@ function scrollActive(){
 		}
 	})
 }
+
+const sr = ScrollReveal({
+  origin: 'top',
+	distance: '80px',
+	duration: 2000,
+	reset: true
+})
+
+sr.reveal('.home-title', {})
+sr.reveal('.home-scroll', {delay: 200});
+sr.reveal('.home-img', {origin: 'right', delay: 400});
+
+sr.reveal('.about-img', {delay: 500});
+sr.reveal('.about-subtitle', { delay: 300 });
+sr.reveal('.about-profession', { delay: 400 });
+sr.reveal('.about-text', { delay: 500 });
+sr.reveal('.about-social-icon', { delay: 600, interval: 200 });
+
+sr.reveal('.skills-subtitle', {});
+sr.reveal('.skills-name', {distance: '20px', delay: 50, interval: 100});
+sr.reveal('.skills-img', {delay: 400});
+
+sr.reveal('.portfolio-img', { interval: 200 });
